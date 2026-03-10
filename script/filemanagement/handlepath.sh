34b02106d6397ae3a24537337da2853b0b14b16f
vk datta <134008395+vkdatta@users.noreply.github.com>
2026-03-10 10:58:28 +0530

    Update handlepath.sh

--- script/filemanagement/handlepath.sh
+++ script/filemanagement/handlepath.sh
@@ -11,3 +11,21 @@ get_abs_path() {
         return 1
     fi
 }
+init_path() {
+    if [ $# -eq 1 ]; then
+        target="$1"
+
+        if [ -d "$target" ]; then
+            path=$(get_abs_path "$target")
+
+        elif [ -f "$target" ]; then
+            selected=$(get_abs_path "$target")
+            handle_file "$selected"
+            exit $?
+
+        else
+            echo "❌ Error: '$target' not found" >&2
+            exit 1
+        fi
+    fi
+}
